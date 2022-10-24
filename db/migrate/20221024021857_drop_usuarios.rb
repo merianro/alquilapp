fb23aff0f24ceacb26b55a1ff197797936e9c7c4
jeroratusny <jeroratusny@gmail.com>
2022-10-23 23:20:25 -0300

    arreglo db

---
+++ db/migrate/20221024021857_drop_usuarios.rb
@@ -0,0 +1,5 @@
+class DropUsuarios < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :usuarios
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_020042) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_021857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "cars", force: :cascade do |t|
-    t.string "marca"
-    t.string "modelo"
-    t.integer "ano"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "supervisors", force: :cascade do |t|
-    t.string "nombre"
-    t.string "apellido"
-    t.integer "dni"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_supervisors_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_supervisors_on_reset_password_token", unique: true
-  end
 
   create_table "sus", force: :cascade do |t|
     t.string "name"
@@ -69,23 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_020042) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "usuarios", force: :cascade do |t|
-    t.string "nombre"
-    t.string "apellido"
-    t.date "fdn"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.boolean "admin"
-    t.index ["email"], name: "index_usuarios_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
 
 end
