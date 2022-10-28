5a8ea094bbf441b17e85092cdc241961affaa202
Tomm-Mc <mcnallytomas@gmail.com>
2022-10-28 18:34:15 -0300

    Mejoras en las vistas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  enum role: [:user, :su, :admin]
+  enum role: [:driver, :su, :admin]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,19 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+user = User.create! :name => 'admin', :email => 'admin@g.com', :role => 2, :password => '123123', :password_confirmation => '123123'
+user2 = User.create! :name => 'su', :email => 'su@g.com', :role => 1, :password => '123123', :password_confirmation => '123123'
+user3 = User.create! :name => 'user', :email => 'user@g.com', :role => 0, :password => '123123', :password_confirmation => '123123'
+
+cars = Car.create([
+            {marca: "Ferrari", modelo:"SF90", ano:"2022", color:"Rojo", transmision:"Automatica", capacidad:"2",ubication:"-34.91157291234968, -57.94059819120864",tipo:"Superauto",disponible: true},
+            {marca: "Ferrari", modelo:"SF90", ano:"2022", color:"Rojo", transmision:"Automatica", capacidad:"2",ubication:"-34.92220742287483, -57.97210323127062",tipo:"Superauto",disponible: true},
+            {marca: "Ferrari", modelo:"SF90", ano:"2022", color:"Rojo", transmision:"Automatica", capacidad:"2",ubication:"-34.921488750606564, -57.956536581327406",tipo:"Superauto",disponible: true},
+            {marca: "Ferrari", modelo:"SF90", ano:"2022", color:"Rojo", transmision:"Automatica", capacidad:"2",ubication:"-34.92054009357561, -57.9305220447106",tipo:"Superauto",disponible: true},
+            {marca: "Ferrari", modelo:"SF90", ano:"2022", color:"Rojo", transmision:"Automatica", capacidad:"2",ubication:"-34.93383933999637, -57.965570292991714",tipo:"Superauto",disponible: true},
+            {marca: "Ferrari", modelo:"SF90", ano:"2022", color:"Rojo", transmision:"Automatica", capacidad:"2",ubication:"-34.932396841058896, -57.93801864135309",tipo:"Superauto",disponible: true},
+            {marca: "Ferrari", modelo:"SF90", ano:"2022", color:"Rojo", transmision:"Automatica", capacidad:"2",ubication:"-34.90746242571045, -57.945803702558536",tipo:"Superauto",disponible: true},
+            {marca: "Ferrari", modelo:"SF90", ano:"2022", color:"Rojo", transmision:"Automatica", capacidad:"2",ubication:"-34.90935624484942, -57.95476038647663",tipo:"Superauto",disponible: true},
+            {marca: "Ferrari", modelo:"SF90", ano:"2022", color:"Rojo", transmision:"Automatica", capacidad:"2",ubication:"-34.911440903653826, -57.96802207196195",tipo:"Superauto",disponible: true},
+            {marca: "Ferrari", modelo:"SF90", ano:"2022", color:"Rojo", transmision:"Automatica", capacidad:"2",ubication:"-34.91369987647464, -57.94841265387437",tipo:"Superauto",disponible: true}
+        ])
