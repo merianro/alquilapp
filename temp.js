b0dabafa2e164954e6097983f789a4c59d58ef66
Mariano Rodriguez <rodriguezmesamariano@gmail.com>
2022-11-01 22:12:45 -0300

    Mapa con puntos dinamicos.

---
+++ temp.js
@@ -0,0 +1,21 @@
+
+        function carMap() {
+            var arreglo = "[
+                #&lt;
+                    Car id: 2, 
+                    marca: &quot;Ferrari&quot;, 
+                    modelo: &quot;SF90&quot;, 
+                    ano: &quot;2022&quot;, 
+                    color: &quot;Rojo&quot;, 
+                    transmision: &quot;Automatica&quot;, 
+                    capacidad: 2, 
+                    created_at: &quot;2022-10-26 19:32:11.290396000 +0000&quot;, 
+                    updated_at: &quot;2022-10-26 19:32:11.290396000 +0000&quot;, 
+                    ubication: #&lt;struct ActiveRecord::Point x=-34.92220742287483, y=-57.97210323127062&gt;, 
+                    tipo: &quot;Superauto&quot;, disponible: true&gt;, 
+                console.log(arreglo.indexOf("x="))
+            return 1;
+        };
+        carMap();
+        //new Mapkick.Map("map", carMap());
+    
