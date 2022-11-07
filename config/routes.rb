1c34b80602b2a78359ba91dbf615828f965c5d45
jeroratusny <jeroratusny@gmail.com>
2022-11-06 22:20:57 -0300

    fixed mp v2

--- config/routes.rb
+++ config/routes.rb
@@ -6,25 +6,26 @@ Rails.application.routes.draw do
   get 'main/home', to: 'home#main'
   devise_for :users
 
+  
   get 'users/suindex', to: 'users#suindex'
   post 'users/suindex', to: 'users#suindex'
 
   get 'users/drindex', to: 'users#drindex'
   post 'users/drindex', to: 'users#drindex'
-
+  
   post '/cars/:id/edit', to: 'cars#edit'
   post '/cars/new', to: 'cars#new'
-
+  
   post '/users/:id/edit', to: 'users#edit'
   post '/users/new', to: 'users#new'
 
   get 'users/anadir_saldo', to: 'users#anadir_saldo'
   post 'users/anadir_saldo', to: 'users#anadir_saldo'
-
-
+  
+  
   get 'users/newsu', to: 'users#newsu'
   post 'users/newsu', to: 'users#newsu'
-
+  
   get 'mp_webhooks', to: 'mp_webhooks#index'
   post "mp_webhooks", to: "mp_webhooks#create"
 
@@ -33,8 +34,13 @@ Rails.application.routes.draw do
   resources :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
+  authenticated :user do
+    root "cars#home", as: :authenticated_root
+  end
+  
   root "main#home"
-
+  
+  
 end
--- db/migrate/20221101221632_add_id_to_validacions.rb
+++
@@ -1,5 +0,0 @@
-class AddIdToValidacions < ActiveRecord::Migration[7.0]
-  def change
-    add_column :validacions, :idS, :integer
-  end
-end
