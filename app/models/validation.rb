c7f8c4d158283589ede3a33afde4ad75e76be979
Mariano Rodriguez <rodriguezmesamariano@gmail.com>
2022-11-08 23:37:07 -0300

    Se trabajo en vistas de usuario, supervisor, etc. Se añadio apartado de licencia al perfil de usuarii

--- app/models/validation.rb
+++ app/models/validation.rb
@@ -2,4 +2,8 @@ class Validation < ApplicationRecord
     has_one_attached :image
     belongs_to :user
     validates_uniqueness_of :user_id
+
+    def procesar
+        image.variant.rezise_to_limit[10,10].processed
+    end
 end
