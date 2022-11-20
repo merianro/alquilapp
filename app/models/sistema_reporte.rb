88a269fec997690836e5a77cf5894e15b59dab0b
jeroratusny <jeroratusny@gmail.com>
2022-11-20 14:24:32 -0300

    pequeños errores

--- app/models/sistema_reporte.rb
+++ app/models/sistema_reporte.rb
@@ -1,6 +1,8 @@
 class SistemaReporte < ApplicationRecord
   has_many_attached :images
+  validates :images, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..(5.megabytes) }
   belongs_to :user
   enum severidad: [:leve, :moderado, :grave]
+
 
 end
