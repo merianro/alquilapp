024d540985819bad3245306520d573429cbd9ba6
jeroratusny <jeroratusny@gmail.com>
2022-11-22 18:41:01 -0300

    validaciones alquiler arregladas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_20_234522) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_203137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -146,6 +146,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_234522) do
     t.integer "role", default: 0
     t.bigint "phone"
     t.date "vencimiento_licencia"
+    t.point "location_point"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
