c8570068e0127c9318bc82fb3634c1ac30f862f4
Tomm-Mc <mcnallytomas@gmail.com>
2022-12-07 12:08:56 -0300

    Chequeo Parametros

--- app/controllers/parametros_controller.rb
+++ app/controllers/parametros_controller.rb
@@ -21,30 +21,45 @@ class ParametrosController < ApplicationController
 
   # POST /parametros or /parametros.json
   def create
-    @parametro = Parametro.new(parametro_params)
+      @parametro = Parametro.new(parametro_params)
 
-    respond_to do |format|
-      if @parametro.save
-        format.html { redirect_to parametros_url, notice: "Parametro creado con exito!." }
-        format.json { render :index, status: :created, location: @parametro }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @parametro.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @parametro.save
+          format.html { redirect_to parametros_url, notice: "Parametro creado con exito!." }
+          format.json { render :index, status: :created, location: @parametro }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @parametro.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # PATCH/PUT /parametros/1 or /parametros/1.json
-  def update
-    respond_to do |format|
-      if @parametro.update(parametro_params)
-        format.html { redirect_to  parametros_url, notice: "Parametros actualizadas correctamente." }
-        format.json { render :index, status: :ok, location: @parametro }
+    # PATCH/PUT /parametros/1 or /parametros/1.json
+    def update
+      aux = params[:parametro][:monto]
+
+      if aux.scan(/\D/).empty? then
+        respond_to do |format|
+          format.html { redirect_to parametros_url, alert: "Se debe ingresar un numero" }
+          format.json { head :no_content }
+        end
+      elsif aux.to_i > 0 then
+        respond_to do |format|
+          if @parametro.update(parametro_params)
+            format.html { redirect_to  parametros_url, notice: "Parametros actualizadas correctamente." }
+            format.json { render :index, status: :ok, location: @parametro }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @parametro.errors, status: :unprocessable_entity }
+          end
+        end
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @parametro.errors, status: :unprocessable_entity }
+        respond_to do |format|
+          format.html { redirect_to parametros_url, alert: "No se puede dar un valor negativo al parametro" }
+          format.json { head :no_content }
+        end
       end
-    end
+
   end
 
   # DELETE /parametros/1 or /parametros/1.json
--- app/models/parametro.rb
+++ app/models/parametro.rb
@@ -1,3 +1,4 @@
 class Parametro < ApplicationRecord
   enum tipo: [:Multa, :Tarifa] 
+  validates :monto, numericality: {only_integer: true}
 end
